eb1b7392ae839772d87c7c040b0739290481a3b3
ardiman <pc.ardiman98@gmail.com>
2023-04-11 11:59:40 +0800

    initialize app

---
+++ components/layouts/sidebar.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { BiLogOut } from 'react-icons/bi';
+import { BsHouseFill, BsBellFill } from 'react-icons/bs';
+import { FaUser } from 'react-icons/fa';
+
+import SidebarItem from './sidebarItem';
+import SidebarLogo from './sidebarLogo';
+import SidebarTweetButton from './sidebarTweetButton';
+
+const Sidebar = () => {
+
+  const items = [
+    {
+      icon: BsHouseFill,
+      label: 'Home',
+      href: '/',
+    },
+    {
+      icon: BsBellFill,
+      label: 'Notifications',
+      href: '/notifications',
+      auth: true,
+      // alert: currentUser?.hasNotification
+    },
+    {
+      icon: FaUser,
+      label: 'Profile',
+      // href: `/users/${currentUser?.id}`,
+      auth: true,
+    },
+  ]
+
+  return (
+    <div className="col-span-1 h-full pr-4 md:pr-6">
+        <div className="flex flex-col items-end">
+          <div className="space-y-2 lg:w-[230px]">
+            <SidebarLogo />
+            {items.map((item) => (
+              <SidebarItem
+                key={item.href}
+                alert={item.alert}
+                auth={item.auth}
+                href={item.href} 
+                icon={item.icon} 
+                label={item.label}
+              />
+            ))}
+            {/* {currentUser && <SidebarItem onClick={() => signOut()} icon={BiLogOut} label="Logout" />} */}
+            <SidebarTweetButton />
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default Sidebar
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
+import Layout from '@/components/layout'
+import LoginModal from '@/components/modals/loginModal'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <LoginModal />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  )
 }
